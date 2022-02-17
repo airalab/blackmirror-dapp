e899b42cbc69efb94188576cc6d1572f42dcd4ec
vol4tim <vol4tim@gmail.com>
2022-02-17 21:25:16 +0400

    add address subscription

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
   endpoint: "wss://kusama.rpc.robonomics.network/",
   isDevelopmentAccounts: false,
-  subscription: "",
+  subscription: "4GgRRojuoQwKCZP9wkB69ZxJY4JprmHtpzEzqJLjnqu4jk1r",
   stream:
     "https://www.youtube.com/embed/z4JHrwdhlAQ?autoplay=1&mute=1&controls=0"
 };
