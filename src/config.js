4ea7a0be0a8a796611fe4a60906405e2effdb739
vol4tim <vol4tim@gmail.com>
2022-02-18 16:10:49 +0400

    update stream youtube

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ export default {
   isDevelopmentAccounts: false,
   subscription: "4GgRRojuoQwKCZP9wkB69ZxJY4JprmHtpzEzqJLjnqu4jk1r",
   stream:
-    "https://www.youtube.com/embed/z4JHrwdhlAQ?autoplay=1&mute=1&controls=0",
+    "https://www.youtube.com/embed/live_stream?channel=UCkemsNJWaCmvF1Oi50C-hAg&autoplay=1&mute=1&controls=0",
   discord: "https://discord.com/invite/5UWNGNaAUf"
 };
