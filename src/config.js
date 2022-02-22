e77742012b8578a3295abad3886f6872f2b01e1e
vol4tim <vol4tim@gmail.com>
2022-02-22 12:54:28 +0400

    revert stream

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ export default {
   isDevelopmentAccounts: false,
   subscription: "4GgRRojuoQwKCZP9wkB69ZxJY4JprmHtpzEzqJLjnqu4jk1r",
   stream:
-    "https://www.youtube.com/embed/AMX7TWVbmgs?autoplay=1&mute=1&controls=0",
+    "https://www.youtube.com/embed/live_stream?channel=UCrSiho1uB-1n6F8cZpCLhjQ&autoplay=1&mute=1&controls=0",
   discord: "https://discord.com/invite/5UWNGNaAUf"
 };
